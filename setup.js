fcf4d39bc7c197384759addc3cce94e90fa2753f
Mikolaj <m.iksrigz@gmail.com>
2023-06-13 00:41:39 +0200

    asd

--- setup.js
+++ setup.js
@@ -12,57 +12,84 @@ const createMenu = () => {
     const button = document.createElement('button');
     button.setAttribute('id', `button${i}`);
     button.setAttribute('class', 'button');
-    button.setAttribute('onclick', `removeMenu(${(i + 2) * 4})`);
+    button.setAttribute('onclick', `prepareCards(${(i + 2) * 4})`);
     button.textContent = `4 X ${i + 2}`;
     board.appendChild(button);
   }
 }; createMenu();
 
-/*************
- removing menu
- *************/
-
-const removeMenu = numberOfCards => {
-  const menu = board.children;
-  for (let i = 0; i < menu.length; i++) {
-    menu[i].setAttribute('class', 'display-none');
-  }
-  board.setAttribute('class', 'board')
-  preparingCards(numberOfCards);
-}
 
 /***************
  preparing cards
  ***************/
 
-const preparingCards = numberOfCards => {
-  const availableCards = {};
-  for (let i = 1; i <= numberOfCards; i++) {
-    if (i <= numberOfCards / 2) {
-      availableCards[`card${i}`] = { cardValue: i, cardImgUrl: `url(img/img${i}.png)` };
-    } else {
-      let value = i - numberOfCards / 2;
-      availableCards[`card${i}`] = { cardValue: value, cardImgUrl: `url(img/img${value}.png)` };
-    }
+const prepareCards = numberOfCards => {
+  const avaivableCards = {};
+  for (let i = 0; i < numberOfCards / 2; i++) {
+    avaivableCards[`card${i + 1}`] = { 'value': i + 1, amountLeft: 2 };
   }
-  shufflingCards(availableCards, numberOfCards);
+  removeMenu();
+  shuffleCards(avaivableCards, numberOfCards);
+}
+
+/*************
+removing menu
+*************/
+
+const removeMenu = () => {
+  const menu = board.children;
+  for (let i = 0; i < menu.length; i++) {
+    menu[i].setAttribute('class', 'display-none');
+  }
+  board.classList.add('board');
 }
 
 /***************
  shuffling cards
  ***************/
 
-const shufflingCards = (cardsObj, numberOfCards) => {
-
-  for (let i = 1; i < numberOfCards; i++) {
-    let { cardValue, cardImgUrl } = cardsObj[`card${i}`]
-    console.log(cardValue);
-    console.log(cardImgUrl);
+const shuffleCards = (avaivableCards, numberOfCards) => {
+  const shuffledCards = [];
+  for (let i = 0; i < numberOfCards; i++) {
+    let procede = true;
+    while (procede) {
+      let randomNumber = Math.floor(Math.random() * (numberOfCards / 2)) + 1;
+      if (avaivableCards[`card${randomNumber}`].amountLeft > 0) {
+        let cardValue = avaivableCards[`card${randomNumber}`].value;
+        shuffledCards.push(cardValue);
+        avaivableCards[`card${randomNumber}`].amountLeft--;
+        procede = false
+      }
+    }
   }
-  // for (let obj in cardsObj) {
-  //   const card = document.createElement('div');
-  //   card.setAttribute('id', `${obj}`)
-  //   card.style.backgroundImage = cardsObj[obj].cardImgUrl;
-  //   board.appendChild(card);
-  // }
+  drawBoard(shuffledCards);
+}
+
+/*************
+ drawing board
+ ************/
+
+const drawBoard = shuffledCards => {
+  for (let i = 0; i < shuffledCards.length; i++) {
+    const card = document.createElement('div');
+    card.setAttribute('id', `card${i + 1}`);
+    card.setAttribute('class', `card-back`);
+    card.setAttribute('onclick', `revealCard(${i + 1}, ${shuffledCards[i]})`);
+    board.appendChild(card);
+  }
+}
+
+/**************
+ revealing card
+ **************/
+
+const revealCard = (id, nr) => {
+  const card = document.querySelector(`#card${id}`);
+  card.style.backgroundImage = `url(img/img${nr}.png)`;
+  setTimeout(`hideCard(${id})`, '1000');
+}
+
+const hideCard = id => {
+  const card = document.querySelector(`#card${id}`);
+  card.style.backgroundImage = 'url(img/crowSmoking.png)';
 }
