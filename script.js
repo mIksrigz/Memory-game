895ffd10b6cea8a1a64dbbfa7f310383b214874f
Mikolaj <m.iksrigz@gmail.com>
2023-06-18 02:44:03 +0200

    asd

--- script.js
+++ script.js
@@ -1,128 +1,132 @@
 const board = document.querySelector('#board');
 
-const StartingMenuContainer = document.createElement('div');
-StartingMenuContainer.classList.add('starting-menu-container');
-board.append(StartingMenuContainer);
+const startingMenuContainer = document.createElement('div');
+startingMenuContainer.classList.add('starting-menu-container');
+board.append(startingMenuContainer);
 
 const instruction = document.createElement('h2');
-instruction.id = 'instruction';
 instruction.classList.add('instruction');
-instruction.innerText = 'Choose Board Size';
-StartingMenuContainer.append(instruction);
+instruction.innerText = 'Choose board size';
+startingMenuContainer.append(instruction);
 
 const buttonsContainer = document.createElement('div');
-buttonsContainer.classList.add('buttons-container')
-StartingMenuContainer.append(buttonsContainer);
+buttonsContainer.classList.add('buttons-container');
+startingMenuContainer.append(buttonsContainer);
 
-for (let i = 3; i < 6; i++) {
+for (let i = 0; i < 3; i++) {
+
   const boardSizeButton = document.createElement('button');
-  boardSizeButton.id = `bord-size-button${i - 2}`;
   boardSizeButton.classList.add('board-size-button');
-  boardSizeButton.value = i;
-  boardSizeButton.innerText = `${i} X 4`;
+  boardSizeButton.innerText = `${i + 3} X 4`;
+  boardSizeButton.value = (i + 3) * 4;
   buttonsContainer.append(boardSizeButton);
+
 }
+
+
+
+
+let cards = [];
+const shuffledCards = [];
+
+
 
 board.addEventListener('click', e => {
-  if (e.target.matches('button')) {
-    const rows = e.target.value;
-    StartingMenuContainer.remove();
-    const cardQuantity = rows * 4;
-    drawBoard(cardQuantity);
-  }
+  if (e.target.matches('button')) prepareCards(e.target.value);
 })
 
-const drawBoard = cardQuantity => {
-  for (let i = 0; i < cardQuantity; i++) {
-    const card = document.createElement('div');
-    card.id = `card${i + 1}`;
-    card.classList.add('card')
-    board.append(card);
-  }
-  board.classList.add(`collumns-4`)
-  prepareCards(cardQuantity);
-}
 
 
-const availableCards = [];
-const prepareCards = cardQuantity => {
 
-  for (let i = 0; i < cardQuantity / 2; i++) {
-    availableCards.push({ cardId: i, cardsLeft: 2 });
+const prepareCards = number => {
+  for (let i = 0; i < (number / 2); i++) {
+    cards.push({ cardUrl: `url(img/img${i + 1}.png)`, cardsLeft: 2 });
   }
   shuffleCards();
 }
 
-const shuflledcards = [];
 const shuffleCards = () => {
-  const pairsQuantity = availableCards.length;
-  const cardsQuantity = pairsQuantity * 2;
 
-  for (let i = 0; i < cardsQuantity; i++) {
+  for (let i = 0; i < cards.length * 2; i++) {
 
-    let procede = true;
+    let procede = true
 
     while (procede) {
-      let randomIndex = Math.floor(Math.random() * pairsQuantity);
-      let leftToDraw = availableCards[randomIndex].cardsLeft;
 
-      if (leftToDraw > 0) {
-        availableCards[randomIndex].cardsLeft--;
-        const id = availableCards[randomIndex].cardId;
-        const url = `url(img/img${id + 1}.png)`;
+      let randomIndex = Math.floor(Math.random() * cards.length);
+      let { cardUrl, cardsLeft } = cards[randomIndex];
 
-        shuflledcards.push({ cardUrl: url });
+      if (cardsLeft > 0) {
+        shuffledCards.push(cardUrl);
+        cards[randomIndex].cardsLeft--;
         procede = false;
       }
     }
-
   }
+  cards = [];
+  removeInstructions();
+}
+
+const removeInstructions = () => {
+  startingMenuContainer.classList.add('disply-none');
+  drawBoard();
+}
+
+const drawBoard = () => {
+  board.classList.add('collumns-4');
+  shuffledCards.forEach((card, index) => {
+    const cardElement = document.createElement('div');
+    cardElement.id = index;
+    cardElement.classList.add('card');
+    board.append(cardElement);
+  })
+
+  board.addEventListener('click', e => {
+    if (e.target.matches('div')) revealCard(e.target);
+  });
 }
 
 let revealdCards = 0;
-let revealdCardsArray = [];
-let revealdCardsUrl = [];
-
-board.addEventListener('click', e => {
-
-  if (e.target.matches('div')) {
-
-    if (revealdCards < 2) {
-      revealCard(e.target);
-
-    }
-
-  }
-});
+let revealdCardsArray = []
+let pairsFound = 0;
 
 const revealCard = card => {
-  let id = card.id;
-  let index = id.match(/\d+/)[0] - 1;
-  card.style.backgroundImage = shuflledcards[index].cardUrl;
-  revealdCardsUrl.push(shuflledcards[index].cardUrl);
-  revealdCardsArray.push(card);
-  revealdCards++;
 
-  if (revealdCards === 2) {
-    if (revealdCardsUrl[0] === revealdCardsUrl[1]) {
-      revealdCardsArray[0].style.opacity = '0';
-      revealdCardsArray[1].style.opacity = '0';
-      revealdCardsArray = [];
-      revealdCardsUrl = [];
-      revealdCards = 0;
-    } else {
-      console.log(revealdCardsArray)
-      setTimeout('hideCards(revealdCardsArray)', '1000');
+  if (card.style.opacity !== 0) {
+
+    if (revealdCards < 2) {
+      revealdCardsArray.push(card);
+      let url = shuffledCards[card.id];
+      card.style.backgroundImage = `${url}`;
+      revealdCards++;
+
+    }
+    if (revealdCards === 2) {
+      setTimeout('checkIfPair(revealdCardsArray)', '1000');
     }
 
+  } else {
+    console.log('already found pair');
+  }
+
+}
+
+const checkIfPair = cards => {
+  if (shuffledCards[cards[0].id] === shuffledCards[cards[1].id]) {
+    cards[0].style.opacity = '0';
+    cards[1].style.opacity = '0';
+    cards[0].style.cursor = 'default';
+    cards[1].style.cursor = 'default';
+    revealdCardsArray = [];
+    revealdCards = 0;
+  } else {
+    hideCard(revealdCardsArray);
   }
 }
 
-const hideCards = revealdCardsArr => {
-  revealdCardsArr[0].style.backgroundImage = 'url(img/crowSmoking.png)';
-  revealdCardsArr[1].style.backgroundImage = 'url(img/crowSmoking.png)';
+const hideCard = cards => {
+  cards[0].style.backgroundImage = 'url(img/crowSmoking.png)';
+  cards[1].style.backgroundImage = 'url(img/crowSmoking.png)';
   revealdCardsArray = [];
-  revealdCardsUrl = [];
   revealdCards = 0;
 }
-
